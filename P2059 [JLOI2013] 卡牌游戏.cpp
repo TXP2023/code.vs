940e33acb99468324564659f9075b2e5c8432388
新沛 谭 <bobi20110114@outlook.com>
2024-07-18 15:52:40 +0800

    增加模板 增加P2059

---
+++ P2059 [JLOI2013] 卡牌游戏.cpp
@@ -0,0 +1,39 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <ctype.h>
+
+typedef long long ll;
+typedef unsigned long long unill;
+
+//函数前向声明
+//快读函数
+template< typename T >
+inline T readf();
+
+int main()
+{
+    return 0;
+}
+
+template< typename T >
+inline T readf()
+{
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch))
+    {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
