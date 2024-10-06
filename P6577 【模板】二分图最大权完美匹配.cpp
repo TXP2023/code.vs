102968f7fea5222f2fb84cb03903056e5eb6588d
TXP2023 <bobi20110114@outlook.com>
2024-10-06 17:03:48 +0800

    AA

---
+++ P6577 【模板】二分图最大权完美匹配.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <ctype.h>
+#include <climits>
+#include <stdint.h>
+
+typedef int64_t ll;
+typedef uint64_t unill;
+
+//函数前向声明
+//快读函数
+template< typename T >
+inline T readf();
+
+int main() {
+    freopen(".in", "r", stdin);
+
+    return 0;
+}
+
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
