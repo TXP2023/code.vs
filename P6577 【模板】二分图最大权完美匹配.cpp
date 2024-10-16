d096eb0667cd2b0446daa8b723a2cf52a675c6e1
TXP2023 <bobi20110114@outlook.com>
2024-10-16 22:10:55 +0800

    P6577

--- P6577 【模板】二分图最大权完美匹配.cpp
+++ P6577 【模板】二分图最大权完美匹配.cpp
@@ -13,8 +13,49 @@ typedef uint64_t unill;
 template< typename T >
 inline T readf();
 
+std::vector< std::vector< ll > >  graph;
+std::vector< ll >  object; // object[i]为右部点i所匹配的左部点
+std::vector< ll > ltag, rtag;// 左部点顶标 left-tag 右部点顶标 right-tag
+std::vector< ll > pre; //?
+std::vector< ll > slack;
+std::vector< bool > rb; // right-bool 即右部点i是否已经分配
+ll n, m;
+
+inline void match(ll u/*左部图点u*/) {
+    const ll inf = std::numeric_limits< ll >::min();
+    ll l, r = 0, min_match_v = 0, min_match;
+    std::fill(pre.begin(), pre.end(), 0);
+    std::fill(slack.begin(), slack.end(), inf);
+
+    object[r] = u;
+    while (true) {
+        
+    }
+}
+
+inline ll Kuhn_Munkres() {
+    //初始化
+    object.resize(n, -1);
+    ltag.resize(n, 0);
+    rtag.resize(n, 0);
+    pre.resize(n);
+
+    for (size_t i = 0; i < n; i++) {
+
+    }
+}
+
 int main() {
     freopen(".in", "r", stdin);
+
+    n = readf< ll >(), m = readf< ll >();
+
+    graph.resize(n, std::vector< ll >(n, 0)); //graph[i][j]为左部点i与右部点j之间的权值
+    for (size_t i = 0; i < m; i++){
+        ll u1 = readf< ll >(), v2 = readf< ll >(), w = readf< ll >();
+        graph[u1 - 1][v2 - 1] = w;
+    }
+
 
     return 0;
 }
