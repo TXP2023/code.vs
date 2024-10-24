fdc6aa05d19e30958edb5d130570231917866375
TXP2023 <bobi20110114@outlook.com>
2024-10-24 17:14:28 +0800

    p6577

--- P6577 【模板】二分图最大权完美匹配.cpp
+++ P6577 【模板】二分图最大权完美匹配.cpp
@@ -21,24 +21,45 @@ std::vector< ll > Ltop, Rtop; //顶标
 std::vector< ll > pre;
 std::vector< ll > slack;
 std::vector< std::vector< ll > > graph;
-ull n, m;
+ull n, m, ans = 0;
 
-inline void add_Augment(ll u); //增加增广路径
+inline void init(); //初始化
+inline void find_Path(ll u); //增加增广路径
 inline void Kuhn_Munkres(); //KM算法
-inline void replace(ll u); //转移？
+inline void add_Path(ll u); //转移
+
+inline void init() /*初始化*/ {
+    Ltop.resize(n, LLONG_MIN);
+    std::vector< ll >(Ltop).swap(Ltop);
+    Rtop.resize(n, 0);
+    std::vector< ll >(Rtop).swap(Rtop);
+    pre.resize(n, 0);
+    std::vector< ll >(pre).swap(pre);
+    Lmatch.resize(n, -1);
+    std::vector< ll >(Lmatch).swap(Lmatch);
+    Rmatch.resize(n, -1);
+    std::vector< ll >(Rmatch).swap(Rmatch);
+    ltag.resize(n, false);
+    std::vector< bool >(ltag).swap(ltag);
+    rtag.resize(n, false);
+    std::vector< bool >(rtag).swap(rtag);
+    slack.resize(n, 0);
+    std::vector< ll >(slack).swap(slack);
+    graph.resize(n, std::vector< ll >(n, LLONG_MIN));
+}
 
 inline void Kuhn_Munkres() {
-    std::fill(slack.begin(), slack.end(), LLONG_MAX);
-    std::fill(pre.begin(), pre.end(), -1);
-    std::fill(ltag.begin(), ltag.end(), false);
-    std::fill(rtag.begin(), rtag.end(), false);
     for (size_t i = 0/*这里的i是枚举左部图点*/; i < n; i++) {
-        add_Augment(i);
+        std::fill(slack.begin(), slack.end(), LLONG_MAX);
+        //std::fill(pre.begin(), pre.end(), -1);
+        std::fill(ltag.begin(), ltag.end(), false);
+        std::fill(rtag.begin(), rtag.end(), false);
+        find_Path(i);
     }
     return;
 }
 
-inline void add_Augment(ll u) {
+inline void find_Path(ll u) {
     std::queue< ll > que;//左部图点队列
     que.push(u);
 
@@ -52,8 +73,8 @@ inline void add_Augment(ll u) {
                     pre[i] = v;
                     if (slack[i] == 0)/*如果当前不存在差值, 添加增广路*/ {
                         rtag[i] = true;
-                        if (Rmatch[i] == -1) {
-                            replace(i);
+                        if (Rmatch[i] == -1) /*如果这个右部图节点没有被匹配*/ {
+                            add_Path(i);
                             return;
                         }
                         else {
@@ -63,22 +84,45 @@ inline void add_Augment(ll u) {
                 }
             }
         }
-        ll Difference = LLONG_MAX/*找最小差值*/;
+        ll MinDifference = LLONG_MAX/*找最小差值*/;
         for (size_t i = 0; i < n; i++) {
-            if (!rtag[i]) {
-                Difference = std::min(Difference, slack[i]);
+            if (!rtag[i]) /*如果这个右部图点不在相等子图里*/ {
+                MinDifference = std::min(MinDifference, slack[i]);
+            }
+        }
+        for (size_t i = 0; i < n; i++) {
+            if (ltag[i]) {
+                Ltop[i] -= MinDifference;
+            }
+            if (rtag[i]) {
+                Rtop[i] += MinDifference;
+            }
+            else {
+                slack[i] -= MinDifference;
+            }
+        }
+        for (size_t i = 0; i < n; i++) {
+            if (!rtag[i] && slack[i] == 0) /*添加到相等子图*/ {
+                rtag[i] = 1;
+                if (Rmatch[i] == -1) {
+                    add_Path(i);
+                    return;
+                }
+                else {
+                    que.push(Rmatch[i]);
+                }
             }
         }
     }
 }
 
 
-inline void replace(ll u) {
+inline void add_Path(ll u) {
     ll t;
-    while (u) {
+    while (u != -1) {
         t = Rmatch[pre[u]];
         Rmatch[pre[u]] = u;
-        Rmatch[u] = pre[u];
+        Lmatch[u] = pre[u];
         u = t;
     }
     return;
@@ -86,17 +130,14 @@ inline void replace(ll u) {
 
 
 int main() {
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
     n = readf< ull >(), m = readf< ull >();
 
     graph.resize(n, std::vector< ll >(m, 0));
 
-    Ltop.resize(n, LLONG_MIN);
-    Rtop.resize(n, 0);
-    pre.resize(n, 0);
-    Lmatch.resize(n, -1);
-    Rmatch.resize(n, -1);
-    ltag.resize(n, false);
-    rtag.resize(n, false);
+    init();
 
     for (size_t i = 0; i < m; i++) {
         ll u = readf< ll >(), //左部图
@@ -106,7 +147,15 @@ int main() {
         Ltop[u - 1] = std::max(Ltop[u - 1], w);
     }
 
+    Kuhn_Munkres();
 
+    for (size_t i = 0; i < n; i++) {
+        ans += Lmatch[i] + Rmatch[i];
+    }
+    printf("%lld\n", ans);
+    for (size_t i = 0; i < n; i++) {
+        printf("%lld ", Rmatch[i]);
+    }
 
     return 0;
 }
