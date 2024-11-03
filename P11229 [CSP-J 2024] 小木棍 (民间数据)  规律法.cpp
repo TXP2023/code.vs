554f0d7057241a19f08ff88808c5fa87135fe715
TXP2023 <bobi20110114@outlook.com>
2024-11-03 22:41:53 +0800

    P11229

---
+++ P11229 [CSP-J 2024] 小木棍 (民间数据)  规律法.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <ctype.h>
+#include <climits>
+#include <iostream>
+#include <stdint.h>
+
+typedef int64_t ll;
+typedef uint64_t unill;
+
+//函数前向声明
+//快读函数
+template< typename T >
+inline T readf();
+
+template< typename T >
+inline void inputf(T* p);
+
+int main() {
+    freopen("input.txt", "r", stdin);
+
+
+
+    return 0;
+}
+
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+
+template< typename T >
+inline void inputf(T* p) {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    //return sgn ? -ret : ret;
+    *p = (sgn) ? -ret : ret;
+    return;
+#endif
+}
