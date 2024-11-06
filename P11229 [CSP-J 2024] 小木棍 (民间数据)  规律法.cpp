0865865dcef7084efcfa8982a095256a88740479
TXP2023 <bobi20110114@outlook.com>
2024-11-06 18:27:05 +0800

    P11229

--- P11229 [CSP-J 2024] 小木棍 (民间数据)  规律法.cpp
+++ P11229 [CSP-J 2024] 小木棍 (民间数据)  规律法.cpp
@@ -17,10 +17,92 @@ inline T readf();
 template< typename T >
 inline void inputf(T* p);
 
+ll t, n;
+
+inline void def() {
+    switch (n) {
+    case 1:
+        printf("-1\n");
+        break;
+    case 2:
+        printf("1\n");
+        break;
+    case 3:
+        printf("7\n");
+    case 4:
+        printf("4\n");
+        break;
+    case 5:
+        printf("2\n");
+        break;
+    case 6:
+        printf("6\n");
+        break;
+    default:
+        switch (n % 7) {
+        case 0:
+            for (size_t i = 0; i < n; i += 7) {
+                printf("8");
+            }
+            puts("\n");
+            break;
+        case 1:
+            printf("16");
+            for (size_t i = 8; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        case 2:
+            printf("1");
+            for (size_t i = 2; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        case 3:
+            printf("7");
+            for (size_t i = 3; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        case 4:
+            printf("20");
+            for (size_t i = 12; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        case 5:
+            printf("2");
+            for (size_t i = 5; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        case 6:
+            printf("6");
+            for (size_t i = 6; i < n; i += 7) {
+                printf("8");
+            }
+            printf("\n");
+            break;
+        }
+        break;
+    }
+    return;
+}
+
 int main() {
     freopen("input.txt", "r", stdin);
 
+    inputf(&t);
 
+    while (t--) {
+        inputf(&n);
+        def();
+    }
 
     return 0;
 }
